58785851df7cdb6c16bd90df3178f1ae3daaefa5
Lincoln Stein <lstein@gmail.com>
2025-07-11 16:36:10 -0400

    update dependencies and fix search api starter script

--- scripts/start_search_api.sh
+++ scripts/start_search_api.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
 cd $(dirname $0)/..
-PYTHONPATH=./backend uvicorn backend.search_api:app --reload --host 0.0.0.0
+PYTHONPATH=./src/backend uvicorn backend.search_api:app --reload --host 0.0.0.0
 
